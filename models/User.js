41b1cac46ddca32b32815475cba93acebfbfd8cb
Dave Lum <dave.lum@appian.com>
2018-04-20 14:23:21 -0400

    Cherry picked the dev/prod stuff

--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose; // Same as "const Schema = mongoose.Schema;"
 
 const userSchema = new Schema({
-  googleId: String // Why is this valid JavaScript?!
+  googleId: String, // TODO: Why is this valid JavaScript?! ==> "String" is a reserved word, probably?
+  daveId: String
 });
 
 mongoose.model("users", userSchema);
